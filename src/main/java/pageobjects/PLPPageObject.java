d4ec90f352d9972a83e990e9a67fd865fe6b0589
My Name <me@example.com>
2023-01-10 13:27:57 +0100

    commit1

---
+++ src/main/java/pageobjects/PLPPageObject.java
@@ -0,0 +1,22 @@
+package pageobjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class PLPPageObject {
+    
+    public WebDriver driver;
+    
+    @FindBy(xpath = "(//a[@class='addToCart'])[1]")
+    public WebElement quickAddToCart;
+
+   
+    public void cartPayment1(WebDriver driver) throws InterruptedException{
+        hhhhhhhh
+    }
+
+
+}
